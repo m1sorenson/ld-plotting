cd73be14035d87a9498a80ac97b6b1a461d35063
Michael Sorenson <m1sorenson@health.ucsd.edu>
2022-01-20 13:43:23 -0800

    Fixed main.sh log

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo -e "============\nEUR ancestry\n============" > run_locuszoom.log
 bash src/run_locuszoom.sh -a EUR -f config/freeze3_hits.tsv &>> run_locuszoom.log
-echo -e "==============\nTRANS ancestry\n==============" >> run_locuszoom.log
 echo -e "============\nAFR ancestry\n============" >> run_locuszoom.log
 bash src/run_locuszoom.sh -a AFR -f config/tinnitus_hits.tsv &>> run_locuszoom.log
 echo -e "============\nAMR ancestry\n============" >> run_locuszoom.log
